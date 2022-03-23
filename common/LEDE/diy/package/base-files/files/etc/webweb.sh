a9404390df3a6a6eb3ac6b38cf2ff9a0c378e655
cangzair <101318187+cangzair@users.noreply.github.com>
2022-03-24 05:57:39 +0800

    Update webweb.sh

--- common/LEDE/diy/package/base-files/files/etc/webweb.sh
+++ common/LEDE/diy/package/base-files/files/etc/webweb.sh
@@ -4,18 +4,11 @@
 
 cp -Rf /etc/config/network /mnt/network
 
-sed -i '/mp\/luci-/d' /etc/crontabs/root && echo "0 1 * * 1 rm /tmp/luci-*cache > /dev/null 2>&1" >> /etc/crontabs/root
+#sed -i '/mp\/luci-/d' /etc/crontabs/root && echo "0 1 * * 1 rm /tmp/luci-*cache > /dev/null 2>&1" >> /etc/crontabs/root
 
 if [[ `grep -c "x86_64" /etc/openwrt_release` -eq '0' ]]; then
   source /etc/openwrt_release
   sed -i "s/x86_64/${DISTRIB_TARGET}/g" /etc/banner
-fi
-
-if [[ -d /usr/share/AdGuardHome ]] && [[ -f /etc/init.d/AdGuardHome ]]; then
- chmod -R 775 /usr/share/AdGuardHome /etc/init.d/AdGuardHome
-else
-  rm -fr /etc/config/AdGuardHome.yaml
-  rm -fr /etc/AdGuardHome.yaml
 fi
 
 chmod -R 775 /etc/init.d /usr/share
@@ -23,11 +16,6 @@ chmod -R 775 /etc/init.d /usr/share
 if [[ -f /etc/init.d/ddnsto ]]; then
  chmod 775 /etc/init.d/ddnsto
  /etc/init.d/ddnsto enable
-fi
-
-if [[ -d /usr/lib/lua/luci/view/themes/argon ]]; then
-  uci set argon.@global[0].bing_background=0
-  uci commit argon
 fi
 
 rm -rf /etc/networkip
